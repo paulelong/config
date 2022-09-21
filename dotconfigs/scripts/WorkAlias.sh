91e2699baeec1808a7d34b10260fe59522d829c9
Paul Long <plong@axon.com>
2022-09-21 17:20:52 -0400

    Move sections to scripts to separate work/home

---
+++ dotconfigs/scripts/WorkAlias.sh
@@ -0,0 +1,2 @@
+alias ol='f(){ pwsh -c Open-Logs $@ }; f'
+alias mcd='mcdf(){ md $1 && cd $_ }; mcdf'
---
+++ dotconfigs/scripts/WorkExports.sh
@@ -0,0 +1,4 @@
+export PWManagerCommand='/mnt/c/users/plong/bin/bw.exe get password'
+export OpenLogsCombine=0
+export OpenLogsEditor=lnav
+export Toolbox=~/src/Toolbox
